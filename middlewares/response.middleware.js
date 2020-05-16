9a0f41c27ad6c37e89060e1d3055712d84e3140b
Peter Trofimov <ptrofimov7@gmail.com>
2020-05-16 19:41:41 +0300

    Implemented middleware for response

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,20 @@
+const {HTTP_STATUS_ERROR_QUERY, HTTP_STATUS_ERROR_UNKNOWN} = require("../constants/index");
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
-    next();
-}
+  // TODO: Implement middleware that returns result of the query
+  if (res.data) {
+    return res.status("200").json(res.data);
+  }
+
+  if (res.err) {
+    const { message, httpStatus = HTTP_STATUS_ERROR_QUERY } = res.err;
+    return res.status(httpStatus).json({ error: true, message });
+  }
+
+  if (!res.headersSent) {
+    res.status(HTTP_STATUS_ERROR_UNKNOWN).render("UnknownError");
+  }
+
+  next();
+};
 
 exports.responseMiddleware = responseMiddleware;
