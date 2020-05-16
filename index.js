c7bb8fd7a667e13c0bae224319b14587f5392a4f
Peter Trofimov <ptrofimov7@gmail.com>
2020-05-16 19:34:21 +0300

    Corrected using of bodyParser

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./config/db');
 
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const routes = require('./routes/index');
 routes(app);
@@ -15,6 +15,8 @@ routes(app);
 app.use('/', express.static('./client/build'));
 
 const port = 3050;
-app.listen(port, () => {});
+app.listen(port, () => {
+
+});
 
 exports.app = app;
